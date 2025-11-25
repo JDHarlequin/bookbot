83aceb44e72ec9d4fa8748177e5810e96be215d3
JDHarlequin <jdharlequin@gmail.com>
2025-11-25 19:32:08 +0100

    Bookbot v0.5

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from stats import char_counter
+from stats import *
+import sys
 
 def main():
-    char_counter()
-
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        letterbox = char_counter(sys.argv[1])
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {word_counter(sys.argv[1])} total words")
+        print("--------- Character Count -------")
+        for i in range (0, len(letterbox)):
+            helper = letterbox[i]
+            if helper["char"] == " ":
+                print(f'spaces: {helper["num"]}')
+            elif helper["char"] == "\n":
+                continue
+            else:
+                print(f'{helper["char"]}: {helper["num"]}')
+        print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,20 +1,27 @@
+def sorter_helper(c):
+    return c["num"]
+
 def get_book_text(path):
-    with open(path) as f:
+    with open(path, encoding="utf-8-sig") as f:
         file_contents = f.read()
     return file_contents
 
-def word_counter():
-    book = get_book_text("books/frankenstein.txt")
+def word_counter(path):
+    book = get_book_text(path)
     word_count = len(book.split())
     return word_count
 
-def char_counter():
+def char_counter(path):
     ccount = {}
-    book = get_book_text("books/frankenstein.txt")
+    sorted_ccount = []
+    book = get_book_text(path)
     book = book.lower()
     for letter in range(0, len(book)):
         if book[letter] not in ccount:
             ccount[book[letter]] = 1
         else:
             ccount[book[letter]] += 1
-    print(ccount)
+    for key in ccount:
+        sorted_ccount.append({"char": key, "num": ccount[key]})
+    sorted_ccount.sort(reverse=True, key=sorter_helper)
+    return sorted_ccount
